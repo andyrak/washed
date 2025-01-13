b5b97a6bfaf310530845cacc31649cd38714a68b
Andy Rakauskas <andy.rak@gmail.com>
2025-01-13 09:34:38 -0800

    fix: :bug: One more timezone calculation fix

--- src/utils/clientUtils.ts
+++ src/utils/clientUtils.ts
@@ -1,28 +1,20 @@
 import fs from "fs";
 import path from "path";
 
-export const fmtMSS = (seconds: number) => {
+export const getGreeting = () => {
   // Get the user's timezone
   const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  // Convert seconds to milliseconds
-  const milliseconds = seconds * 1000;
-
-  // Create a Date object
-  const date = new Date(milliseconds);
-
-  // Format the time to MM:SS in the user's timezone
-  return new Intl.DateTimeFormat('en-US', {
-    minute: '2-digit',
-    second: '2-digit',
+  // Get the current date and time in the user's timezone
+  const now = new Date();
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    hour12: false,
     timeZone: userTimezone,
-  }).format(date);
-};
+  });
+  const currentHour = parseInt(formatter.format(now), 10);
 
-
-export const getGreeting = () => {
-  const currentHour = new Date().getHours();
-
+  // Determine the greeting based on the hour
   if (currentHour >= 0 && currentHour < 12) {
     return "morning";
   }
